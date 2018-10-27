934f85e95a16002e6dd19191cb6f5609d03f2e75
zan <zan_zibert@hotmail.com>
2018-10-27 15:59:14 +0200

    returning promises

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
-let promise = new Promise(function(resolve, reject) {
+new Promise(function(resolve, reject) {
+
     setTimeout(() => resolve(1), 1000);
-  });
   
-  promise.then(function(result) {
-    alert(result); // 1
-    return result * 2;
-  });
+  }).then(function(result) {
   
-  promise.then(function(result) {
     alert(result); // 1
-    return result * 2;
-  });
   
-  promise.then(function(result) {
-    alert(result); // 1
-    return result * 2;
+    return new Promise((resolve, reject) => { // (*)
+      setTimeout(() => resolve(result * 2), 1000);
+    });
+  
+  }).then(function(result) { // (**)
+  
+    alert(result); // 2
+  
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(result * 2), 1000);
+    });
+  
+  }).then(function(result) {
+  
+    alert(result); // 4
+  
   });
