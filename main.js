805a664a0ae43ee47e77336018a589a10a86331d
zan <zan_zibert@hotmail.com>
2018-10-27 14:55:23 +0200

    The syntax of .then

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ let promise = new Promise(function(resolve, reject) {
     // not taking our time to do the job
     resolve(123); // immediately give the result: 123
   });
+
+promise.then(
+function(result) { /* handle a successful result */ },
+function(error) { /* handle an error */ }
+);
