4f71a479cc0cfccd755b25c74c0c7270c546c469
zan <zan_zibert@hotmail.com>
2018-10-27 14:47:52 +0200

    Promise can call only one resolve or reject

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let promise = new Promise(function(resolve, reject) {
-    // after 1 second signal that the job is finished with an error
-    setTimeout(() => reject(new Error("Whoops!")), 1000);
+    resolve("done");
+  
+    reject(new Error("…")); // ignored
+    setTimeout(() => resolve("…")); // ignored
   });
