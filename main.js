be7ee2624213da151a6784c168f088616798b740
zan <zan_zibert@hotmail.com>
2018-10-27 15:24:53 +0200

    Example: loadScript

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-// an "immediately" resolved Promise
-const executor = resolve => resolve("done!");
-const promise = new Promise(executor);
+function loadScript(src) {
+    return new Promise(function(resolve, reject) {
+      let script = document.createElement('script');
+      script.src = src;
+  
+      script.onload = () => resolve(script);
+      script.onerror = () => reject(new Error("Script load error: " + src));
+  
+      document.head.append(script);
+    });
+  }
 
-promise.then(alert); // this alert shows last (*)
+  let promise = loadScript("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js");
 
-alert("code finished"); // this alert shows first
+  promise.then(
+    script => alert(`${script.src} is loaded!`),
+    error => alert(`Error: ${error.message}`)
+  );
+  
+  promise.then(script => alert('One more handler to do something else!'));
