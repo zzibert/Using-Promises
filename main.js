4a9ea1e1fdd5e96b31e63a8a030eeda6ee5f6949
zan <zan_zibert@hotmail.com>
2018-10-27 15:04:01 +0200

    reaction to a successfuly resolved promise

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let promise = new Promise(function(resolve, reject) {
-    // not taking our time to do the job
-    resolve(123); // immediately give the result: 123
+    setTimeout(() => resolve("done!"), 1000);
   });
-
-promise.then(
-function(result) { /* handle a successful result */ },
-function(error) { /* handle an error */ }
-);
+  
+  // resolve runs the first function in .then
+  promise.then(
+    result => alert(result), // shows "done!" after 1 second
+    error => alert(error) // doesn't run
+  );
