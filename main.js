8838f4d4989a6e2caf9b9b3195e2f097159b332e
zan <zan_zibert@hotmail.com>
2018-10-27 15:08:39 +0200

    interested only in successful completions

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-let promise = new Promise(function(resolve, reject) {
-    setTimeout(() => reject(new Error("Whoops!")), 1000);
+let promise = new Promise(resolve => {
+    setTimeout(() => resolve("done!"), 1000);
   });
   
-  // reject runs the second function in .then
-  promise.then(
-    result => alert(result), // doesn't run
-    error => alert(error) // shows "Error: Whoops!" after 1 second
-  );
+  promise.then(alert); // shows "done!" after 1 second
