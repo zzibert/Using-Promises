9cd8e2e4cd54e32521bee6c6376df1094e04fdea
zan <zan_zibert@hotmail.com>
2018-10-27 15:09:22 +0200

    Interested only in errors

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-let promise = new Promise(resolve => {
-    setTimeout(() => resolve("done!"), 1000);
+let promise = new Promise((resolve, reject) => {
+    setTimeout(() => reject(new Error("Whoops!")), 1000);
   });
   
-  promise.then(alert); // shows "done!" after 1 second
+  // .catch(f) is the same as promise.then(null, f)
+  promise.catch(alert); // shows "Error: Whoops!" after 1 second
