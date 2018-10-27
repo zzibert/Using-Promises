3dc3112d22b2ee82547cb14dc053ebf636589042
zan <zan_zibert@hotmail.com>
2018-10-27 14:46:10 +0200

    Unsuccesfull promise

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 let promise = new Promise(function(resolve, reject) {
-    // the function is executed automatically when the promise is constructed
-  
-    // after 1 second signal that the job is done with the result "done!"
-    setTimeout(() => resolve("done!"), 1000);
+    // after 1 second signal that the job is finished with an error
+    setTimeout(() => reject(new Error("Whoops!")), 1000);
   });
