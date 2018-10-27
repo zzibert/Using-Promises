eb6874d7aa4597863e5f18561981eb1d3c896bc4
zan <zan_zibert@hotmail.com>
2018-10-27 15:05:50 +0200

    Reaction to a rejected promise

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let promise = new Promise(function(resolve, reject) {
-    setTimeout(() => resolve("done!"), 1000);
+    setTimeout(() => reject(new Error("Whoops!")), 1000);
   });
   
-  // resolve runs the first function in .then
+  // reject runs the second function in .then
   promise.then(
-    result => alert(result), // shows "done!" after 1 second
-    error => alert(error) // doesn't run
+    result => alert(result), // doesn't run
+    error => alert(error) // shows "Error: Whoops!" after 1 second
   );
