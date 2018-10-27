dd4d930750a19cf1de68c7f791aaafc890e71794
zan <zan_zibert@hotmail.com>
2018-10-27 14:45:01 +0200

    Succesfull promise

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-let promise = new Promise((resolve, reject) => {
-    //executor
-})
+let promise = new Promise(function(resolve, reject) {
+    // the function is executed automatically when the promise is constructed
+  
+    // after 1 second signal that the job is done with the result "done!"
+    setTimeout(() => resolve("done!"), 1000);
+  });
