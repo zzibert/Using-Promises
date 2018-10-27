b236a78e715da30e1f550af89094f7593711b479
zan <zan_zibert@hotmail.com>
2018-10-27 19:40:33 +0200

    Example of Thenable object

--- main.js
+++ main.js
@@ -1,25 +1,16 @@
-new Promise(function(resolve, reject) {
-
-    setTimeout(() => resolve(1), 1000);
+class Thenable {
+    constructor(num) {
+      this.num = num;
+    }
+    then(resolve, reject) {
+      alert(resolve); // function() { native code }
+      // resolve with this.num*2 after the 1 second
+      setTimeout(() => resolve(this.num * 2), 1000); // (**)
+    }
+  }
   
-  }).then(function(result) {
-  
-    alert(result); // 1
-  
-    return new Promise((resolve, reject) => { // (*)
-      setTimeout(() => resolve(result * 2), 1000);
-    });
-  
-  }).then(function(result) { // (**)
-  
-    alert(result); // 2
-  
-    return new Promise((resolve, reject) => {
-      setTimeout(() => resolve(result * 2), 1000);
-    });
-  
-  }).then(function(result) {
-  
-    alert(result); // 4
-  
-  });
+  new Promise(resolve => resolve(1))
+    .then(result => {
+      return new Thenable(result); // (*)
+    })
+    .then(alert); // shows 2 after 1000ms
