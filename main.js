ba0fd6ad387c716b28c78e5e3466a2f8f37f6dea
zan <zan_zibert@hotmail.com>
2018-10-28 15:41:10 +0100

    Let’s take showAvatar() example from the chapter Promises chaining and rewrite it using async/await

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-async function f() {
+async function showAvatar() {
 
-  let promise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("done!"), 1000)
-  });
+  // read our JSON
+  let response = await fetch('/article/promise-chaining/user.json');
+  let user = await response.json();
 
-  let result = await promise; // wait till the promise resolves (*)
+  // read github user
+  let githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
+  let githubUser = await githubResponse.json();
 
-  console.log(result); // "done!"
+  // show the avatar
+  let img = document.createElement('img');
+  img.src = githubUser.avatar_url;
+  img.className = "promise-avatar-example";
+  document.body.append(img);
+
+  // wait 3 seconds
+  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+
+  img.remove();
+
+  return githubUser;
 }
 
-f();
+showAvatar();
