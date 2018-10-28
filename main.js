38e4038637794de5666d0c51a1b3cd9751f75862
zan <zan_zibert@hotmail.com>
2018-10-28 09:17:48 +0100

    To make the chain extendable, we need to return a promise that resolves when the avatar finishes showing.

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 fetch('/article/promise-chaining/user.json')
-  // Load it as json
   .then(response => response.json())
-  // Make a request to github
   .then(user => fetch(`https://api.github.com/users/${user.name}`))
-  // Load the response as json
   .then(response => response.json())
-  // Show the avatar image (githubUser.avatar_url) for 3 seconds (maybe animate it)
-  .then(githubUser => {
+  .then(githubUser => new Promise(function(resolve, reject) {
     let img = document.createElement('img');
     img.src = githubUser.avatar_url;
     img.className = "promise-avatar-example";
     document.body.append(img);
 
-    setTimeout(() => img.remove(), 3000); // (*)
-  });
+    setTimeout(() => {
+      img.remove();
+      resolve(githubUser);
+    }, 3000);
+  }))
+  // triggers after 3 seconds
+  .then(githubUser => alert(`Finished showing ${githubUser.name}`));
