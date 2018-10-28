982781ef662bb3b8da79e687a5005d71cc990fe1
zan <zan_zibert@hotmail.com>
2018-10-28 15:32:34 +0100

    Async/Await

--- main.js
+++ main.js
@@ -1,21 +1,5 @@
-class HttpError extends Error { // (1)
-  constructor(response) {
-    super(`${response.status} for ${response.url}`);
-    this.name = 'HttpError';
-    this.response = response;
-  }
+async function f() {
+  return 1;
 }
 
-function loadJson(url) { // (2)
-  return fetch(url)
-    .then(response => {
-      if (response.status == 200) {
-        return response.json();
-      } else {
-        throw new HttpError(response);
-      }
-    })
-}
-
-loadJson('no-such-user.json') // (3)
-  .catch(alert); // HttpError: 404 for .../no-such-user.json
+f().then(value => console.log(value)); // 1
