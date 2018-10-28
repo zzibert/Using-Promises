dcc72143147b323b6ac442c0ce33f5bc2b11d16f
zan <zan_zibert@hotmail.com>
2018-10-28 15:43:21 +0100

    await accepts thenables

--- main.js
+++ main.js
@@ -1,25 +1,18 @@
-async function showAvatar() {
+class Thenable {
+  constructor(num) {
+    this.num = num;
+  }
+  then(resolve, reject) {
+    alert(resolve); // function() { native code }
+    // resolve with this.num*2 after 1000ms
+    setTimeout(() => resolve(this.num * 2), 1000); // (*)
+  }
+};
 
-  // read our JSON
-  let response = await fetch('/article/promise-chaining/user.json');
-  let user = await response.json();
-
-  // read github user
-  let githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
-  let githubUser = await githubResponse.json();
-
-  // show the avatar
-  let img = document.createElement('img');
-  img.src = githubUser.avatar_url;
-  img.className = "promise-avatar-example";
-  document.body.append(img);
-
-  // wait 3 seconds
-  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
-
-  img.remove();
-
-  return githubUser;
+async function f() {
+  // waits for 1 second, then result becomes 2
+  let result = await new Thenable(1);
+  alert(result);
 }
 
-showAvatar();
+f();
